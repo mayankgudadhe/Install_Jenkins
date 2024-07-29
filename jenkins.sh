e6bc8591303793ec98828aaaf16b2a6dc7356cb7
mayankgudadhe <mayankgudadhe12345@gmail.com>
2024-07-29 20:50:52 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,5 +4,4 @@ wget -p /root/server https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.91/bin/apache
 unzip /root/server/apache-tomcat-9.0.91.zip -d /root/server/
 chmod -R 777 /root/server/apache-tomcat-9.0.91
 wget -p /root/server/apache-tomcat-9.0.91/webapps/ https://get.jenkins.io/war-stable/2.452.3/jenkins.war
-cd /root/server/apache-tomcat-9.0.91/bin
-./startup.sh
+/root/server/apache-tomcat-9.0.91/bin/startup.sh
