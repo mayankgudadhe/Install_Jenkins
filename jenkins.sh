c4e10c7ffdd65d1d874ba749e93a0c28965d8a43
mayankgudadhe <mayankgudadhe12345@gmail.com>
2024-07-29 20:42:46 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir server
+wget -p /root/server https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.91/bin/apache-tomcat-9.0.91.zip
+unzip /root/server/apache-tomcat-9.0.91.zip -d /root/server/
+chmod -R 777 /root/server/apache-tomcat-9.0.91
+wget -p /root/server/apache-tomcat-9.0.91/webapps/ https://get.jenkins.io/war-stable/2.452.3/jenkins.war
+cd /root/server/apache-tomcat-9.0.91/bin
+./startup.sh
